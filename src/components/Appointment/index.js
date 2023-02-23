99d568e16bbfc813258433b524cde837a4e213b5
Corey Silver <coreysilver@outlook.com>
2023-02-23 01:02:16 -0500

    Completed Appointment index.js page

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <>
+      <Header time={props.time} />
+      <article className="appointment">
+        {props.interview ? (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+          />
+        ) : (
+          <Empty />
+        )}
+      </article>
+    </>
+  );
 }
 
 export default Appointment;
